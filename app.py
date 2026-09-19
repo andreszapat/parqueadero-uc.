98b63cfa5d67fd2e5b5000baa9c9f77ad130fbdd
osinisterra <omar.sinisterra00@usc.edu.co>
2026-09-18 20:02:34 -0500

    Implementa calculo de tarifa segun tipo de usuario

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ visitantes = 0
 
 i = 0
 
-# Ciclo principal
 while i < N and vehiculos_validos < 30:
 
     print("VEHICULO", i + 1, " ")
@@ -28,3 +27,34 @@ while i < N and vehiculos_validos < 30:
     else: 
         if tipo != "E" and tipo != "D" and tipo != "V":
             print("Advertencia: tipo invalido. Se registrara como visitante.")
+            tipo = "v"
+
+        #Tarifas
+        tarifa = 0
+        
+        if tipo == "E":
+
+            estudiantes = estudiantes + 1
+
+            # Primeras 2 horas gratis
+            if horas <= 2:
+                tarifa = 0
+            else:
+                tarifa = (horas - 2) * 800
+
+        elif tipo == "D":
+
+            docentes = docentes + 1
+
+            tarifa = horas * 500
+
+        else:
+
+            visitantes = visitantes + 1
+
+            # Primera hora $1500
+            if horas <= 1:
+                tarifa = 1500
+            else:
+                tarifa = 1500 + ((horas - 1) * 1200)
+
